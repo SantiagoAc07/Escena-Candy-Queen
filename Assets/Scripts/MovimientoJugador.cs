29e404ca28557b31b31b9fee3ed55c302cdd98fc
Santiago Alzate C <salzate361@misena.edu.co>
2024-01-27 05:18:07 -0500

    Se añade movimiento orbital al objeto direccionad
    El objeto cilindro detiene su orbita y da el vector de dirección en el momento de presionar "x", se busca hacer una barra para determinar la potencia del impulso a aplicar en esa dirección, sin embargo no he podido aún encontrar la manera

---
+++ Assets/Scripts/MovimientoJugador.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoJugador : MonoBehaviour
+{
+    public Transform centerOfMass;
+    public float orbitalSpeed;
+    public float scale = 1.0f;
+
+    // Update is called once per frame
+    void Update(){        
+        if(Input.GetKeyDown(KeyCode.X))
+            CalcularVector(); //llama una funcion con la cual se calcula el vector de direccion
+        Orbit();
+
+        
+
+    }
+
+    private void CalcularVector(){
+        //detiene la orbita de la flecha de direccion
+        orbitalSpeed = 0;
+
+        //calcula el vector de direccion
+        Vector3 dir = (transform.position - centerOfMass.position).normalized;
+
+        //le da impulso en la direccion del vector dir aqui cambiaras el 5f por la fuerza que vas a implementar
+        centerOfMass.GetComponent<Rigidbody>().AddForce(dir * 5f, ForceMode.Impulse);
+    }
+
+    //orbita donde se hace el movimiento de nuestro indicador
+    void Orbit(){
+
+        transform.RotateAround(centerOfMass.position, Vector3.up, orbitalSpeed * Time.deltaTime);
+        
+    }
+
+   
+}
