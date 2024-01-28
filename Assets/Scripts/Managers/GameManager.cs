cc992a6d80886791285ebe6af8d9936e03b23bb4
Santiago Alzate C <salzate361@misena.edu.co>
2024-01-28 15:23:16 -0500

    Add Change Scene function

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,16 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void ChangeGameScene()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        SceneManager.LoadScene("Main");
     }
 }
